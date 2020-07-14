a435fd5f3fff62d9bfa2386adcb2b2fda31226d1
Eric Holmstrom <eric.holmstrom.sydney@gmail.com>
2020-07-14 11:50:36 +1000

    models added

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from sqlalchemy import func, ForeignKey
 
 from . import db
 
+MAX_STRING_LENGTH = 65535
+
 
 class User(db.Model):
     __tablename__ = "users"
@@ -14,9 +16,41 @@ class User(db.Model):
         return '<User {}>'.format(self.id)
 
 
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255))
+    perm_level = db.Column(db.Integer)
+    user_id = db.Column(db.String(16), ForeignKey("users.id"))
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.id)
+
+
+class Courses(db.Model):
+    __tablename__ = "courses"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    course = db.Column(db.String(255))
+    msg = db.Column(db.String(MAX_STRING_LENGTH))
+    user_id = db.Column(db.String(16), ForeignKey("users.id"))
+
+    def __repr__(self):
+        return '<Courses {}>'.format(self.id)
+
+
+class AnonMsgs(db.Model):
+    __tablename__ = "anon_msgs"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    msg = db.Column(db.String(MAX_STRING_LENGTH))
+    user_id = db.Column(db.String(16), ForeignKey("users.id"))
+
+    def __repr__(self):
+        return '<AnonMsgs {}>'.format(self.id)
+
+
 class UserProfileDetail(db.Model):
-    __tablename__ = "userprofiledetails"
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "user_profile_details"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.String(16), ForeignKey("users.id"))
     detail_key = db.Column(db.String(32))
     value = db.Column(db.String(256))
@@ -25,3 +59,24 @@ class UserProfileDetail(db.Model):
         return '<UserProfileDetail {}>'.format(self.id)
 
 
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    review = db.Column(db.String(MAX_STRING_LENGTH))
+    user_id = db.Column(db.String(16), ForeignKey("users.id"))
+    lecturer = db.Column(db.String(255))
+    term = db.Column(db.String(255))
+    is_approved = db.Column(db.Boolean())
+
+    def __repr__(self):
+        return '<Review {}>'.format(self.id)
+
+
+class Report(db.Model):
+    __tablename__ = "reports"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    msg_id = db.Column(db.Integer, ForeignKey("anon_msgs.id"))
+    report = db.Column(db.String(MAX_STRING_LENGTH))
+
+    def __repr__(self):
+        return '<AnonMsgs {}>'.format(self.id)
--- app/slack.py
+++ app/slack.py
@@ -21,6 +21,16 @@ def pair():
     return make_response("Pair success", 200)
 
 
+@slack.route("/course", methods=['POST'])
+def get_course_summary():
+    return make_response("Course Summary", 200)
+
+
+@slack.route("/courses", methods=['POST'])
+def get_courses_listing():
+    pass
+
+
 @slack.route('/stylecheck', methods=['POST'])
 def stylecheck():
     if not verify_request(request):
