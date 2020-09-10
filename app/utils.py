1f07ec6765cc5e09dc16b35c39834356d9b676e8
eric <eric.holmstrom.sydney@gmail.com>
2020-09-10 20:10:47 +1000

    minor changes

--- app/utils.py
+++ app/utils.py
@@ -12,6 +12,7 @@ from urllib.parse import quote
 
 from app import db
 from app.models import User, UserProfileDetail, Roles, AnonMsgs, Report, UserRole
+from config import Config
 
 
 def verify_request(request):
@@ -24,7 +25,7 @@ def verify_request(request):
     data = request.get_data()
     slack_signature = request.headers['X-Slack-Signature']
     sig_basestring = str.encode('v0:' + timestamp + ':') + data
-    slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+    slack_signing_secret = Config.SLACK_SIGNING_SECRET
     secret = str.encode(slack_signing_secret)
     my_signature = 'v0=' + hmac.new(key=secret, msg=sig_basestring, digestmod=hashlib.sha256).hexdigest()
     if hmac.compare_digest(my_signature, slack_signature):
